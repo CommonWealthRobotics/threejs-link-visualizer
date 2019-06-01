ee45f74a82c3804bb7dbc1f57dc1254f7a79c105
Alex Camilo <alex.camilo@gmail.com>
2019-05-31 20:58:35 -0400

    Loads OBJs from API and displays them

--- js/display-links.js
+++ js/display-links.js
@@ -4,20 +4,20 @@
 var objexample = "# Group\ng v3d.csg\n\n# Vertices\nv 0.0 2.5 15.5\nv 0.0 -2.4999999999999996 15.5\nv 0.0 -2.5 10.5\nv 0.0 2.5 10.5\nv -32.0 2.5000000000000036 15.5\nv -32.0 2.500000000000004 10.5\nv -32.0 -2.4999999999999964 10.5\nv -32.0 -2.499999999999996 15.5\n\n# Faces\n\n# End Group v3d.csg\n";
 var scene = new THREE.Scene();
 //var loader = new THREE.STLLoader();
-var loader = new THREE.OBJLoader2();
+var loader = new THREE.OBJLoader();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-camera.position.z = 2;
+camera.position.z = 20;
 
 // robot object
 class robotLink {
 	constructor(uri, index) {
 
 		var obj = this;
-		console.log("New Link '" + uri + "': " + index + "");
+		console.log("display-links: New Link '" + uri + "': " + index + "");
 		loader.load(uri, function (j) {
 			obj.addToScene(j);
 		});
@@ -25,21 +25,16 @@ class robotLink {
 		this.index = index;
 	}
 	index = null;
-	object = null;
-	addToScene(loadedObject) {
-		console.log(event);
-		console.log("Object " + this.index + " loaded");
-		var myobj = loadedObject;
+	sceneobject = null;
+	addToScene(mesh) {
+		console.log("display-links: Object " + this.index + " loaded");
 
-		var material = new THREE.MeshBasicMaterial({
-				color: 0x00ff00
-			});
 		//debugger;
-		myobj.children[0].material = material;
-		debugger;
-		renderer.append(myobj);
-
-		this.object = object;
+		//debugger;
+		mesh.children[0].material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+		this.sceneobject = mesh;
+		//debugger;
+		scene.add(mesh);
 	}
 }
 
@@ -47,7 +42,7 @@ class robot {
 	linkObjects = [];
 	constructor(uri) {
 		var obj = this;
-		console.log("New Robot '" + uri + "'");
+		console.log("display-links: New Robot '" + uri + "'");
 		$.getJSON(uri, function (j) {
 			obj.loadCad(j);
 		});
@@ -55,10 +50,10 @@ class robot {
 	};
 	loadCad(json) {
 		var cad = json.robots[0].cad;
-		console.log("Loading " + cad.length + " CAD objects");
+		console.log("display-links: Loading " + cad.length + " CAD objects");
 
 		for (var i = 0; i < cad.length; i++) {
-			console.log("Adding Link " + i);
+			console.log("display-links: Adding Link " + i);
 			var robotlink = new robotLink(cad[i], i);
 			this.linkObjects.push(robotlink);
 
